10b28709efb439f923cddd2b9e061716d786587b
MFraund <30523772+MFraund@users.noreply.github.com>
2022-04-14 00:28:49 -0700

    Beginning the process

---
+++ ManipulatorGUI.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 14 00:23:03 2022
+
+@author: ALS
+"""
+
+import os
+import numpy as np
+import pandas as pd
+
+from pyqt5 import QMainWindow
+
+from matplotlib import pyplot as plt
+
+
+class ManipulatorGUI(QMainWindow):
+	pass
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	
+	gui = ManipulatorGUI()
+	
